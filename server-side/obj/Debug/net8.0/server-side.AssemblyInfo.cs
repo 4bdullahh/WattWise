516dc555cb763127a131e69eacfa5c6bcf948c10
Vinchenzo98 <vinnyboardman@hotmail.co.uk>
2024-10-19 17:48:44 +0100

    assembly files

--- server-side/obj/Debug/net8.0/server-side.AssemblyInfo.cs
+++ server-side/obj/Debug/net8.0/server-side.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server-side")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b411ee4c4a7b6f1a74858fd213971023324b9ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dda0b444e5f63fa5cfc0f10d861e032e559dbfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("server-side")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server-side")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
