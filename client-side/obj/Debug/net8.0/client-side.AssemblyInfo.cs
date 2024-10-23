9b4af1ce681796e76bbf1600f25b0ada433720d6
MuhammadMamoon-1 <144691503+MuhammadMamoon-1@users.noreply.github.com>
2024-10-23 09:48:57 +0100

    trasnfer

--- client-side/obj/Debug/net8.0/client-side.AssemblyInfo.cs
+++ client-side/obj/Debug/net8.0/client-side.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("client-side")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b492bfea0226fb12eecebd286f1152a07505e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae595d4d75b4c0d541f87bdd724bc58e6aa52e78")]
 [assembly: System.Reflection.AssemblyProductAttribute("client-side")]
 [assembly: System.Reflection.AssemblyTitleAttribute("client-side")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- server-side/obj/Debug/net8.0/server-side.AssemblyInfo.cs
+++ server-side/obj/Debug/net8.0/server-side.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server-side")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b492bfea0226fb12eecebd286f1152a07505e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae595d4d75b4c0d541f87bdd724bc58e6aa52e78")]
 [assembly: System.Reflection.AssemblyProductAttribute("server-side")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server-side")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
